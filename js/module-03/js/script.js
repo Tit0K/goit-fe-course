ee4556f6b8e1c5a01b2d3e6851284d5479ba838d
Ivan Titiomir <221519van@gmail.com>
2021-08-09 16:22:14 +0300

    module-03

---
+++ js/module-03/js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+const loginsArr = ['botee', 'qwerty', 'user18'];
+
+const isLoginValid = (login) => {
+    if(login.length >=4 && login.length <=16) {
+        return true;
+    } else return false;
+};
+
+const isLoginUnique = (allLogins, login) => {
+    return allLogins.includes(login);
+};
+
+const addLogin = (allLogins, login) => {
+    if(isLoginValid(login)) {
+        if(!isLoginUnique(loginsArr, login)) {
+            loginsArr.push(login);
+            console.log('Логин успешно добавлен!');
+        } else {
+            console.log('Такой логин уже используется!');
+        }
+    } else {
+        console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+    }
+};
+
+addLogin(loginsArr, 'bote2');
