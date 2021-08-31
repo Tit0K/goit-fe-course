42c884194c3a41b0b25f39f2647304acba05828b
Ivan Titiomir <221519van@gmail.com>
2021-08-31 15:35:15 +0300

    2 last task fix

--- js/module-07/js/dop-1.js
+++ js/module-07/js/dop-1.js
@@ -1,13 +1,21 @@
-const getUniqueSkills = users =>
-  users
-    .reduce((skillsList, user) => {
-      user.skills.forEach(skill => {
-        if (!skillsList.includes(skill)) {
-          skillsList.push(skill);
-        }
-      });
-      return skillsList;
-    }, [])
-    .sort();
+let getSkillsList = (skillsList, user) => {
+  user.skills.map(skill => {
+    if (!skillsList.includes(skill)) {
+      skillsList.push(skill);
+    }
+  });
+  return skillsList;
+};
+
+const getUniqueSkills = users => users.reduce(getSkillsList, []).sort();
 
 console.log(getUniqueSkills(users));
+
+let arg = (a, b) => {
+  return a.friends.length - b.friends.length;
+};
+
+const getNamesSortedByFriendsCount = users =>
+  users.sort(arg).map(user => user.name);
+
+console.log(getNamesSortedByFriendsCount(users));
--- js/module-07/js/main.js
+++
@@ -1,6 +0,0 @@
-const getNamesSortedByFriendsCount = users =>
-  users
-    .sort((a, b) => a.friends.length - b.friends.length)
-    .map(user => user.name);
-
-console.log(getNamesSortedByFriendsCount(users));
